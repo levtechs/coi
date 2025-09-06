417e6f523be09e2de1e2cd8892a894e09d942676
Lev <smolsky.lev@gmail.com>
2025-09-06 16:00:58 -0400

    fixed layout issues

--- app/components/editor/editor.tsx
+++ app/components/editor/editor.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useState } from "react";
-
 import { Project, Card } from "@/lib/types";
-
 import Modal from "../modal";
 import MenuBar from "./menu/menu";
 import ContentPanel from "./content";
@@ -30,51 +28,50 @@ const Editor = ({
     setTitle,
 }: EditorProps) => {
     const [tab, setTab] = useState<"content" | "cards">("content"); // "content" or "cards"
-    const [chatToggled, setChatToggled] = useState(true);
-
     const [modalContents, setModalContents] = useState(noModal);
     const closeModal = () => setModalContents(noModal);
-
     const [cardPopup, setCardPopup] = useState<Card | null>(null);
 
     return (
-        <div className="p-6 mx-4 sm:mx-6 lg:mx-12 w-auto bg-[var(--neutral-100)] rounded-lg shadow-lg mt-8">
-            {/* Top Menu Bar */}
-            <MenuBar
-                project={project}
-                user={user}
-                setProject={setProject}
-                addCollaborator={addCollaborator}
-                setTitle={setTitle}
-                setModalContents={setModalContents}
-            />
+        <div className="flex flex-col h-dvh w-full bg-[var(--neutral-100)] text-[var(--foreground)]">
+            {/* Main Layout Container */}
+            <div className="flex flex-col w-full h-full p-6">
+                
+                {/* Top Menu Bar */}
+                <MenuBar
+                    project={project}
+                    user={user}
+                    setProject={setProject}
+                    addCollaborator={addCollaborator}
+                    setTitle={setTitle}
+                    setModalContents={setModalContents}
+                />
 
-            {/* Main Layout */}
-            <div className="relative mt-4">
-                {/* Main Content Panel - occupies the available space with a right margin */}
-                <div className={`mr-80 md:mr-96 ${chatToggled ? "max-w-[50vw]": "w-full"} transition-all duration-300`}>
-                    <TabSelector
-                        tabs={["content", "cards"]}
-                        onTabChange={(tabName) => setTab(tabName as "content" | "cards")}
-                    />
-                    <ContentPanel
-                        project={project}
-                        hidden={tab !== "content"}
-                    />
-                    <CardsPanel 
-                        project={project} 
-                        onCardClick={(card: Card) => setCardPopup(card)}
-                        hidden={tab === "content"}
-                    />
+                {/* Main Content Area */}
+                <div className="relative mt-4 flex flex-row flex-1 w-full overflow-hidden">
+                    {/* Main Content Panel - occupies the available space */}
+                    <div className={`flex flex-col flex-1 h-full overflow-y-auto transition-all duration-300`}>
+                        <TabSelector
+                            tabs={["content", "cards"]}
+                            onTabChange={(tabName) => setTab(tabName as "content" | "cards")}
+                        />
+                        <div className="h-4" > 
+                            <ContentPanel
+                                project={project}
+                                hidden={tab !== "content"}
+                            />
+                            <CardsPanel 
+                                project={project} 
+                                onCardClick={(card: Card) => setCardPopup(card)}
+                                hidden={tab === "content"}
+                            />
+                        </div>
 
-                </div>
+                    </div>
 
-                {/* Chat Panel - fixed to the viewport and always centered */}
-                <div className="fixed top-1/2 -translate-y-1/2 right-6">
                     <ChatPanel 
                         project={project} 
                         setProject={setProject}
-                        toggleChat={setChatToggled} 
                     />
                 </div>
             </div>
