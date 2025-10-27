1358f12642acac20c2e77aeafe8bc4d115fc0c5a
Lev Smolsky <lev@levspan-2.local>
2025-10-27 14:05:39 -0400

    login prompt on course page

--- app/components/login_prompt.tsx
+++ app/components/login_prompt.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/navigation";
 import Button from "./button";
 
 interface LoginPromptParams {
@@ -5,11 +6,13 @@ interface LoginPromptParams {
 }
 
 const LoginPrompt = ({ page }: LoginPromptParams) => {
+    const router = useRouter();
+
     return (
         <div className="flex flex-col items-center justify-center h-screen p-6 bg-[var(--background)] text-[var(--foreground)] text-center">
             <p className="text-2xl font-semibold mb-4">You are not logged in.</p>
             <p className="text-lg mb-6">Please log in to access {page}.</p>
-            <Button color="var(--accent-500)" onClick={() => (window.location.href = "/login")}>
+            <Button color="var(--accent-500)" onClick={() => router.push(`/login?forward=${encodeURIComponent(window.location.pathname)}`)}>
                 Go to Login Page
             </Button>
         </div>
